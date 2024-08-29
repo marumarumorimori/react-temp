f13b535b5893ea80c189eb2d3b1927d1b45b0259
s.tsuji <s.tsuji@fenrir.co.jp>
2024-08-29 23:04:49 +0900

    s

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./index.css";
 
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById("root")!).render(
